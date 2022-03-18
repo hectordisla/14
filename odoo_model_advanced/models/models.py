ecd6f4039d62577763d2f9e19072191e54c36e6a
Hector W. Disla <h.disla@patridgeconsulting.com>
2022-03-18 12:12:18 -0400

    Validacion Global en models.py

--- odoo_model_advanced/models/models.py
+++ odoo_model_advanced/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from logging import exception
 from odoo import models, fields, api, exceptions
 
 
@@ -17,15 +18,24 @@ class Car(models.Model):
        # Parametros nombre-restriccion a nivel de sql-mensaje
        ('number_plate_unique','UNIQUE(number_plate)','El número de matricula debe ser único.'),
     ]
-    #Validacion de campo
-    @api.constrains('cv')
-    def _validate_cv(self):
-        if self.cv <= 0:
-            raise exceptions.ValidationError('Los Caballos de fuerza, no pueden estar en 0.')
-
-    @api.constrains('name')
-    def _validate_name(self):
+    
+    #Validacion
+    @api.constrains('cv','name')
+    def _validate(self):
         if self.name == "":
             raise exceptions.ValidationError('El nombre del modelo no puede estar en blanco.')
+        elif self.cv <= 0:
+            raise exceptions.ValidationError('Los Caballos de fuerza, no pueden estar en 0.')
+    
+    # #Validacion de campo
+    # @api.constrains('cv')
+    # def _validate_cv(self):
+    #     if self.cv <= 0:
+    #         raise exceptions.ValidationError('Los Caballos de fuerza, no pueden estar en 0.')
 
+    # @api.constrains('name')
+    # def _validate_name(self):
+    #     if self.name == "":
+    #         raise exceptions.ValidationError('El nombre del modelo no puede estar en blanco.')
 
+    #@api.onchange('fuel_litres')
